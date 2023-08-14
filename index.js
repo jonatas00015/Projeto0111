509127bda2b7d19582a763d02b7d05a675630217
jonatas00015 <101048134+jonatas00015@users.noreply.github.com>
2023-08-14 18:33:44 -0300

    v

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-console.log("oi")
-var clima = "quente"
-console.log(clima)
+//console.log("oi")
+//var clima = "quente"
+//console.log(clima)
+
+//declaração de variavel
+//var name = "jhon"
+
+//agrupamento de declarações
+//let age, ishuman
+//age = 19
+//isHuman = true
+
+//multiplos argumentos na função
+//console.log (name, age, isHuman)
+
+//escrita de testo + variavel/ contenação
+//console.log ('o ' + name + ' tem ' + age + ' anos.')
+//ou interpolar:
+//console.log (`o ${name} tem ${age} anos.`) 
+
+//object
+
+//const person = {
+    //name: 'jon',
+    //age: 20,
+   // wheight: 72.30,
+    //isAdimin: true
+//}
+//console.log (person.age)
+ //ou
+ //console.log (`jon ${person.wheight}kg com ${person.age}`)
+
+ //Array
+ const animals = [
+    'lion',
+    'tiger',
+    'fish'
+ ]
+
+//acessar valores do array
+console.log (animals[0])
