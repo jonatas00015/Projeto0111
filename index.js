19f0386fbab507921b05a1af3aa3c1fb46217b66
jonatas00015 <101048134+jonatas00015@users.noreply.github.com>
2023-08-18 14:38:41 -0300

    oioo

--- index.js
+++ index.js
@@ -31,11 +31,50 @@
  //console.log (`jon ${person.wheight}kg com ${person.age}`)
 
  //Array
- const animals = [
-    'lion',
-    'tiger',
-    'fish'
- ]
+ //const animals = [
+   // 'lion',
+   // 'tiger',
+   // 'fish'
+ //]
 
 //acessar valores do array
-console.log (animals[0])
+//console.log (animals[0])
+
+
+//Exercicio 
+
+//1-var nome = 'weight'
+//2-string
+//3-
+//var name, age, stars, isSubcribed
+//name = 'jhon'
+//age = 19
+//stars = 70
+//isSubcribed = false
+//console.log (name)
+//4- 
+//let student = {
+   // name: 'jhon',
+   // age: 19,
+   // stars: 70,
+   // isSubcribed: false,
+//}
+//console.log (`${student.name } de idade ${student.age} pesa ${student.stars}
+//kg`)
+//5-6-7-
+
+const student = {
+    name:'mike',
+}
+
+const students = [
+    'jhon',
+    student,
+     '17',
+     '72,30',
+     'false'
+]
+console.log (students[1])
+
+
+
